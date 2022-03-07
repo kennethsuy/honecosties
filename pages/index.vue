a7bc8cb873f9ba368fd6b131c27870acd28436d1
Kenneth Uy <1kenneth11@gmail.com>
2022-03-07 00:31:26 -0500

    functional deployment 3/06 without carbon

--- pages/index.vue
+++ pages/index.vue
@@ -92,7 +92,7 @@
         <section class="py-2">
           <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
             <div class="w-1/3">
-              <p>Boost Bonus</p>
+              <p>Boost Bonus Per Item (%)</p>
               <input
                 class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
                 type="number"
@@ -108,15 +108,30 @@
               >
             </div>
           </div>
-          <div hidden>{{ this.calculateBoostCost.toLocaleString("en-US") }}</div>
+           <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
+            <div class="w-1/3">
+              <div class="flex flex-row justify-between">
+                <div class="text-md font-bold">Cost per attempt with boosts</div>
+                <div>{{ this.calculateBoostAttempt.toLocaleString("en-US") }}</div>
+              </div>
+            </div>
+            <div class="w-1/3">
+              <div class="flex flex-row justify-between">
+                <div class="text-md font-bold">Cost to succeed with boosts</div>
+                <div>{{ this.calculateBoostCost.toLocaleString("en-US") }}</div>
+              </div>
+            </div>
+          </div>
+          <div class="w-1/3">
+              <div class="flex flex-row justify-between">
+                <div class="text-md font-bold" v-if="this.calculateBoostCost < this.calculateHoneSuccessCost">Gold saved per boost</div>
+                <div class="text-md font-bold" v-if="this.calculateBoostCost >= this.calculateHoneSuccessCost">Gold LOST per boost</div>
+                <div>{{ this.calculateGoldSaved.toLocaleString("en-US") }}</div>
+              </div>
+            </div>
         </section>
       </div>
     </main>
-    <p hidden>
-      <button style="display: inline-block" @click.prevent="test()">
-        Test Button
-      </button>
-    </p>
   </div>
 </template>
 
@@ -177,6 +192,12 @@ export default {
     },
     calculateBoostCost() {
       return parseInt(this.boostCost * 100 / this.boostBonus);
+    },
+    calculateBoostAttempt(){
+      return (this.boostCost * 100) / this.boostBonus *0.01 * this.honeChance;
+    },
+    calculateGoldSaved(){
+      return ((this.calculateHoneSuccessCost - this.calculateBoostCost)*0.01 * this.boostBonus)
     },
   },
 };
