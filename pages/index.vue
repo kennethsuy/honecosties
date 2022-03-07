a17e642aa344ffbe316cc2a9c049665396f059f1
Alvin Yuan <alvinhyuan@gmail.com>
2022-03-06 23:01:26 -0500

    Added TailwindCSS and made basic UI updates to gear honing table

--- pages/index.vue
+++ pages/index.vue
@@ -1,117 +1,122 @@
 <template>
-  <div id="mainpage">
-    <p>Honing Cost Calculator</p>
+  <div id="mainpage" class="min-h-screen antialiased bg-gray-100">
+    <main class="container mx-auto py-8" role="main">
+      <div class="max-w-2xl mx-auto bg-gray-200 rounded-lg shadow-md">
+        <div class="py-2 w-full mx-auto text-center bg-gray-300 rounded-t-lg border-b border-solid border-gray-400">
+          <div class="text-xl font-bold">Honing Cost Calculator</div>
+        </div>
+        <section class="py-2 border-b border-solid border-gray-400">
+          <div>
+            <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
+              <div class="w-1/3">
+                <p>Stone Requirement</p>
+                <input
+                  class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                  type="number"
+                  v-model="stoneReq"
+                >
+              </div>
+              <div class="w-1/3">
+                <p>Stone Cost (per 10)</p>
+                <input
+                  class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                  type="number"
+                  v-model="stoneCost"
+                >
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
+              <div class="w-1/3">
+                <p>Leapstone Requirement</p>
+                <input
+                  class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                  type="number"
+                  v-model="leapReq"
+                >
+              </div>
+              <div class="w-1/3">
+                <p>Leapstone Cost</p>
+                <input
+                  class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                  type="number"
+                  v-model="leapCost"
+                >
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
+              <div class="w-1/3">
+                <p>Shard Requirement</p>
+                <input
+                  class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                  type="number"
+                  v-model="shardReq"
+                >
+              </div>
+              <div class="w-1/3">
+                <p>Shard Cost (per 1000)</p>
+                <input
+                  class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                  type="number"
+                  v-model="shardCost"
+                >
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="py-2 border-b border-solid border-gray-400">
+          <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
+            <div class="w-1/3">
+              <p>Base Honing Chance (0-100)</p>
+              <input
+                class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                type="number"
+                v-model="honeChance"
+              >
+            </div>
+            <div class="py-2 w-1/3 flex flex-col justify-between">
+              <div class="flex flex-row justify-between">
+                <div class="text-md font-bold">Cost per attempt</div>
+                <div>{{ this.calculateHoneAttemptCost.toLocaleString("en-US") }}</div>
+              </div>
+              <div class="flex flex-row justify-between">
+                <div class="text-md font-bold">Cost to succeed</div>
+                <div>{{ this.calculateHoneSuccessCost.toLocaleString("en-US") }}</div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="py-2">
+          <div class="px-2 py-4 w-full flex flex-wrap gap-x-12 justify-center">
+            <div class="w-1/3">
+              <p>Boost Bonus</p>
+              <input
+                class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                type="number"
+                v-model="boostBonus"
+              >
+            </div>
+            <div class="w-1/3">
+              <p>Boost Cost</p>
+              <input
+                class="px-4 py-2 w-full rounded-lg border border-gray-400 focus:border-green-600 focus:outline-none transition"
+                type="number"
+                v-model="boostCost"
+              >
+            </div>
+          </div>
+          <div hidden>{{ this.calculateBoostCost.toLocaleString("en-US") }}</div>
+        </section>
+      </div>
+    </main>
     <p hidden>
       <button style="display: inline-block" @click.prevent="test()">
         Test Button
       </button>
     </p>
-    <br />
-    <div class="costs">
-      <cv-number-input
-        v-model="stoneCost"
-        label="Stone Cost (bundle of 10)"
-        placeholder="Make it a number"
-      >
-        <template v-if="!isNumeric(this.stoneCost)" slot="invalid-message"
-          >Invalid message text</template
-        >
-      </cv-number-input>
-      <cv-number-input
-        v-model="leapCost"
-        label="Leapstone Cost"
-        placeholder="Make it a number"
-      >
-        <template v-if="!isNumeric(this.leapCost)" slot="invalid-message"
-          >Invalid message text</template
-        >
-      </cv-number-input>
-      <cv-number-input
-        v-model="shardCost"
-        label="Shard Cost (M)"
-        placeholder="Make it a number"
-      >
-        <template v-if="!isNumeric(this.shardCost)" slot="invalid-message"
-          >Invalid message text</template
-        >
-      </cv-number-input>
-    </div>
-    <cv-number-input
-      v-model="stoneReq"
-      label="Stone Req"
-      placeholder="Make it a number"
-    >
-      <template v-if="!isNumeric(this.stoneReq)" slot="invalid-message"
-        >Invalid message text</template
-      >
-    </cv-number-input>
-    <cv-number-input
-      v-model="leapReq"
-      label="Leapstone Req"
-      placeholder="Make it a number"
-    >
-      <template v-if="!isNumeric(this.leapReq)" slot="invalid-message"
-        >Invalid message text</template
-      >
-    </cv-number-input>
-    <cv-number-input
-      v-model="shardReq"
-      label="Shard Req"
-      placeholder="Make it a number"
-    >
-      <template v-if="!isNumeric(this.shardReq)" slot="invalid-message"
-        >Invalid message text</template
-      >
-    </cv-number-input>
-    <cv-number-input
-      v-model="honeChance"
-      label="Hone Chance (between 0 and 100)"
-      placeholder="Make it a number"
-      min=0
-      max=100
-    >
-      <template
-        v-if="
-          !(
-            isNumeric(this.honeChance) &&
-            parseFloat(this.honeChance) <= 100 &&
-            parseFloat(this.honeChance) > 0
-          )
-        "
-        slot="invalid-message"
-        >Must be between 100 and 0</template
-      >
-    </cv-number-input>
-    <div>{{ this.calculateHoneCost }}</div>
-    <cv-number-input
-      v-model="boostCost"
-      label="Boost Cost"
-      placeholder="Make it a number"
-    >
-      <template v-if="!isNumeric(this.boostCost)" slot="invalid-message"
-        >Invalid message text</template
-      >
-    </cv-number-input>
-    <cv-number-input
-      v-model="boostBonus"
-      label="Boost Bonus (between 0 and 100)"
-      placeholder="Make it a number"
-      min=0
-      max=100
-    >
-      <template
-        v-if="
-          !(
-            isNumeric(this.boostBonus) &&
-            parseFloat(this.boostBonus) <= 100 &&
-            parseFloat(this.boostBonus) > 0
-          )
-        "
-        slot="invalid-message"
-        >Must be between 100 and 0</template
-      >
-    </cv-number-input>
-    <div>{{ this.calculateBoostCost }}</div>
   </div>
 </template>
 
@@ -134,35 +139,44 @@ export default {
   },
   data() {
     return {
-      stoneCost: "53",
-      leapCost: "171",
-      shardCost: "240",
       stoneReq: "258",
+      stoneCost: "53",
       leapReq: "8",
+      leapCost: "171",
       shardReq: "60",
+      shardCost: "240",
       honeChance: "40",
       boostCost:"70",
       boostBonus:"0.84"
-
-
     };
   },
   computed: {
-    calculateHoneCost() {
-      let finalCalc =
-        (((parseFloat(this.stoneReq) * parseFloat(this.stoneCost)) / 10 +
-          parseFloat(this.leapReq) * parseFloat(this.leapCost) +
-          (parseFloat(this.shardReq) * parseFloat(this.shardCost)) / 1000) *
-          100) /
-        parseFloat(this.honeChance);
-      if (finalCalc) {
-        return "Cost per hone success is: " + parseFloat(finalCalc);
+    calculateHoneAttemptCost() {
+      let cost = ((
+        (parseFloat(this.stoneReq) * parseFloat(this.stoneCost)) / 10 +
+        parseFloat(this.leapReq) * parseFloat(this.leapCost) +
+        (parseFloat(this.shardReq) * parseFloat(this.shardCost)) / 1000)
+      );
+      if (cost) {
+        return parseInt(cost);
       } else {
-        return "One of your values isn't a number";
+        return "Invalid input";
+      }
+    },
+    calculateHoneSuccessCost() {
+      let cost = ((
+        (parseFloat(this.stoneReq) * parseFloat(this.stoneCost)) / 10 +
+        parseFloat(this.leapReq) * parseFloat(this.leapCost) +
+        (parseFloat(this.shardReq) * parseFloat(this.shardCost)) / 1000) * 100
+      ) / parseFloat(this.honeChance);
+      if (cost) {
+        return parseInt(cost);
+      } else {
+        return "Invalid input";
       }
     },
     calculateBoostCost() {
-      return (this.boostCost * 100 / this.boostBonus)
+      return parseInt(this.boostCost * 100 / this.boostBonus);
     },
   },
 };
